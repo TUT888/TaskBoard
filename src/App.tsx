60ca1c132a6c2d770b6b7823e3677bd7831a6cd4
Tat Uyen Tam <tattam192@gmail.com>
2026-04-17 14:28:52 +0700

    complete add task function

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
 import "./App.css";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
 
+const initialTasks = [
+  { id: 1, title: 'Learn React', status: 'todo' },
+  { id: 2, title: 'Build Todo App', status: 'in-progress' },
+  { id: 3, title: 'Review PR', status: 'done' },
+];
+
 function App() {
+  const [lastId, setLastId] = useState(3);
+  const [tasks, setTasks] = useState(initialTasks)
+
+  const handleAddTask = (title: string, status: string) => {
+    const newTask = {
+      id: lastId + 1,
+      title,
+      status
+    }
+    setTasks([...tasks, newTask])
+    setLastId(lastId + 1)
+  }
+
+  console.log(tasks);
   return (
     <>
       <section>
@@ -19,7 +40,7 @@ function App() {
 
         <div>
           <h2>Task Form</h2>
-          <TaskForm />
+          <TaskForm onAddTask={handleAddTask}/>
         </div>
       </section>
 
