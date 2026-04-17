7554477e54d406482e03a5bd770492ab547962c2
Tat Uyen Tam <tattam192@gmail.com>
2026-04-17 13:55:48 +0700

    create base layout for task list and task form

---
+++ src/components/TaskList.tsx
@@ -0,0 +1,11 @@
+function TaskList() {
+  return (
+    <ul className="task-list">
+      <li>Task 1</li>
+      <li>Task 2</li>
+      <li>Task 3</li>
+    </ul>
+  );
+}
+
+export default TaskList;
